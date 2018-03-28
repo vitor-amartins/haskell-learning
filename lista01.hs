182ce7c92a9b01813c4513418322806b57ab1e4a
Vitor Matheus de Azevedo Martins <vmam@WINDOWS.CIN.UFPE.BR>
2018-03-28 12:40:23 -0300

    Started q1

--- lista01.hs
+++ lista01.hs
@@ -2,7 +2,16 @@
 
 -------------------------------- Question 1 --------------------------------
 
-kSmallest :: [Int] -> Int -> [Int]
-kSmallest [] _ = []
-kSmallest _ 0 = []
-kSmallest l n = 
+--kSmallest :: [Int] -> Int -> [Int]
+--kSmallest [] _ = []
+--kSmallest _ 0 = []
+--kSmallest (a:at) n = 
+
+minList :: [Int] -> Int
+minList [a] = a
+minList (a:at) = min a (minList at)
+
+removeElement :: [Int] -> Int -> [Int]
+removeElement [] _ = []
+removeElement (a:at) n | a == n = at
+					   | otherwise = a :removeElement at 
