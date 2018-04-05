f7390952cd0cf35faa32833ece56be9cab160a4b
vitor-amartins <vmam@cin.ufpe.br>
2018-04-05 09:57:55 -0300

    Started question 3

--- lista02.hs
+++ lista02.hs
@@ -23,3 +23,49 @@ ftest x = x*x - 1
 
 ftest2 :: Double -> Double
 ftest2 x = x*x -3*x + 2
+
+-------------------------------- Question 3 --------------------------------
+
+a = [["The Shape of Water", "Dunkirk", "Get Out"],
+     ["Get Out", "Dunkirk", "The Shape of Water"],
+     ["Dunkirk", "The Shape of Water", "Get Out"]]
+
+-- Pegar uma lista com os filmes
+getListMovies :: [[String]] -> [String] -- head input
+getListMovies (a:at) = a
+-- Qnts filmes tem, i.e., quantas posições possíveis
+qntPos :: [String] -> Int
+qntPos l = length l
+-- (Filme, vetor qntd de vezes q apareceu na posição do índice)
+-- ("Get Out", [1, 0, 2])
+-- ("Dunkirk", [1, 2, 0])
+-- ("The Shape of Water", [1, 1, 1])
+vezesPos :: [String] -> [[String]] -> [(String, [Int])]
+
+
+
+
+first :: [[String]] -> [[(String, Int)]]
+first (a:at) = map zipList (a:at)
+
+zipList :: [String] -> [(String, Int)]    
+zipList l = zip l [0..]
+
+
+movP :: [String] -> [[(String, Int)]] -> [(String, [Int])]
+movP [] _ = []
+movP (a:at) l = (a, moviePositions l a) : movP at l
+
+moviePositions :: [[(String, Int)]] -> String -> [Int]
+moviePositions [] _ = []
+moviePositions (a:at) movie = [movieSum2 a movie] ++ (moviePositions at movie)
+
+movieSum2 :: [(String, Int)] -> String -> Int
+movieSum2 [] _ = 0
+movieSum2 (a:at) movie | movie == fst a = snd a
+                       | otherwise = movieSum2 at movie
+
+countPositions :: (String, [Int]) -> Int -> Int
+countPositions (movie, []) _ = 0
+countPositions (movie, (a:at)) n | a == n = 1 + countPositions (movie, at) n
+                                 | otherwise = countPositions (movie, at) n
